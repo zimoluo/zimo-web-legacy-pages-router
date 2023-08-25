7d6ad2ae98b7a94c790fd6e113ac35a602a0dc88
zimoluo <zimo_luo@outlook.com>
2023-08-26 00:14:51 +0800

    fixed zimo bg

--- components/BackgroundAnimations/ZimoBackgroundAnimation.tsx
+++ components/BackgroundAnimations/ZimoBackgroundAnimation.tsx
@@ -6,8 +6,10 @@ const ZimoBackgroundAnimation = () => {
       <div className="fixed inset-0 -z-10 flex items-center justify-center animate-move-bg-1 pointer-events-none">
         <Image 
           src="/zimo-bg-light-moving-1.svg" 
-          layout="fill" 
-          className="object-cover" 
+          height="0"
+          width="0"
+          layout="fixed"
+          className="object-cover w-full h-full" 
           alt="Background moving image 1"
         />
       </div>
@@ -15,8 +17,10 @@ const ZimoBackgroundAnimation = () => {
       <div className="fixed inset-0 -z-10 flex items-center justify-center animate-move-bg-3 pointer-events-none">
         <Image 
           src="/zimo-bg-light-moving-3.svg" 
-          layout="fill" 
-          className="object-cover" 
+          height="0"
+          width="0"
+          layout="fixed"
+          className="object-cover w-full h-full" 
           alt="Background moving image 3"
         />
       </div>
@@ -24,8 +28,9 @@ const ZimoBackgroundAnimation = () => {
       <div className="absolute inset-0 -z-10 top-4 hidden md:block pointer-events-none">
         <Image 
           src="/zimo-text-light.svg" 
-          layout="fill" 
-          className="object-cover" 
+          height="0"
+          width="0"
+          className="object-cover w-full h-auto" 
           alt="Background text light for desktop"
         />
       </div>
@@ -33,8 +38,9 @@ const ZimoBackgroundAnimation = () => {
       <div className="absolute inset-0 -z-10 top-4 md:hidden pointer-events-none">
         <Image 
           src="/zimo-text-light-mobile.svg" 
-          layout="fill" 
-          className="object-cover" 
+          height="0"
+          width="0"
+          className="object-cover w-full h-auto" 
           alt="Background text light for mobile"
         />
       </div>
@@ -42,8 +48,10 @@ const ZimoBackgroundAnimation = () => {
       <div className="fixed inset-0 -z-10 flex items-center justify-center animate-move-bg-2 pointer-events-none">
         <Image 
           src="/zimo-bg-light-moving-2.svg" 
-          layout="fill" 
-          className="object-cover" 
+          height="0"
+          width="0"
+          layout="fixed"
+          className="object-cover w-full h-full" 
           alt="Background moving image 2"
         />
       </div>
