37450b04a9973f9d10967dbdee881b6f519127bc
zimoluo <zimo_luo@outlook.com>
2023-08-31 12:44:09 +0800

    beautify some code

--- lib/blog/util.ts
+++ lib/blog/util.ts
@@ -1,19 +1,19 @@
-import { awsBucketAddress } from '../constants';
+import { awsBucketAddress } from "../constants";
 
 export const getAuthorImageSrc = (authorId: string): string => {
-    if (authorId === 'zimo') {
-      return '/favicon.svg'
-    }
+  if (authorId === "zimo") {
+    return "/favicon.svg";
+  }
 
-    return `${awsBucketAddress}/blog/author/${authorId}`;
+  return `${awsBucketAddress}/blog/author/${authorId}`;
 };
 
 export const getCoverSrc = (coverImage: string, slug: string): string => {
-  if (!(coverImage)) {
-    return '';
+  if (!coverImage) {
+    return "";
   }
 
-  if (!(coverImage.startsWith('/'))) {
+  if (!coverImage.startsWith("/")) {
     return coverImage;
   }
 
@@ -27,21 +27,21 @@ export const readingTime = (content: string) => {
   const wordCount = content.split(/\s/).length;
   const totalMinutes = Math.ceil(wordCount / 238);
 
-  if (totalMinutes >= 1440) { // 1440 minutes in a day
+  if (totalMinutes >= 1440) {
+    // 1440 minutes in a day
     return "You don't wanna read this";
   }
 
   if (totalMinutes >= 60) {
     const hours = Math.floor(totalMinutes / 60);
     const minutes = totalMinutes % 60;
-    
+
     if (minutes === 0) {
-      return `${hours} hr${hours === 1 ? '' : 's'} read`;
+      return `${hours} hr${hours === 1 ? "" : "s"} read`;
     }
-    
-    return `${hours} hr${hours === 1 ? '' : 's'} ${minutes} min read`;
+
+    return `${hours} hr${hours === 1 ? "" : "s"} ${minutes} min read`;
   }
 
   return `${totalMinutes} min read`;
 };
-
