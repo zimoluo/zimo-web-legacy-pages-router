8b5a179a14be5a6fab0dfc311a4fb691e37746cf
zimoluo <zimo_luo@outlook.com>
2023-10-07 23:47:08 +0800

    experimental fix on photos tile grid

--- components/photos/PhotosTileGrid.tsx
+++ components/photos/PhotosTileGrid.tsx
@@ -6,41 +6,85 @@ interface Props {
 }
 
 const PhotosTileGrid: React.FC<Props> = ({ photoEntries }) => {
-  const gridRef = useRef<HTMLDivElement>(null);
+  const gridRefDesktop = useRef<HTMLDivElement>(null);
+  const gridRefMobile = useRef<HTMLDivElement>(null);
   const [isLoaded, setIsLoaded] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(
+    typeof window !== "undefined" ? window.innerWidth : 768
+  );
 
   useEffect(() => {
-    if (gridRef.current) {
-      import("masonry-layout").then((Masonry) => {
-        const columnWidth = window.innerWidth < 768 ? 168 : 288;
+    const handleResize = () => setWindowWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
 
-        new Masonry.default(gridRef.current as Element, {
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  useEffect(() => {
+    let masonryDesktop: any, masonryMobile: any;
+
+    import("masonry-layout").then((Masonry) => {
+      if (windowWidth >= 768 && gridRefDesktop.current) {
+        masonryDesktop = new Masonry.default(gridRefDesktop.current, {
           itemSelector: ".masonry-item",
-          columnWidth: columnWidth,
+          columnWidth: 288,
           gutter: 6,
           fitWidth: true,
         });
-
-        // Set the loaded state to true once Masonry is initialized.
         setIsLoaded(true);
-      });
-    }
-  }, [photoEntries]);
+      } else if (gridRefMobile.current) {
+        masonryMobile = new Masonry.default(gridRefMobile.current, {
+          itemSelector: ".masonry-item",
+          columnWidth: 168,
+          gutter: 6,
+          fitWidth: true,
+        });
+        setIsLoaded(true);
+      }
+    });
+
+    return () => {
+      // Destroy Masonry instances on cleanup
+      masonryDesktop && masonryDesktop.destroy();
+      masonryMobile && masonryMobile.destroy();
+    };
+  }, [photoEntries, windowWidth]);
 
   return (
-    <section
-      className={`flex justify-center mb-16 px-1.5 transition-opacity duration-500 ease-in-out ${
-        isLoaded ? "opacity-100" : "opacity-0 max-h-0 overflow-hidden"
-      }`}
-    >
-      <div ref={gridRef}>
-        {photoEntries.map((photoEntry, index) => (
-          <div key={index} className="masonry-item">
-            <PhotosTile {...photoEntry} />
-          </div>
-        ))}
-      </div>
-    </section>
+    <>
+      <section
+        className={`flex justify-center mb-16 px-1.5 transition-opacity duration-500 ease-in-out ${
+          isLoaded && windowWidth >= 768
+            ? "opacity-100"
+            : "opacity-0 max-h-0 overflow-hidden"
+        }`}
+      >
+        <div ref={gridRefDesktop}>
+          {photoEntries.map((photoEntry, index) => (
+            <div key={index} className="masonry-item">
+              <PhotosTile {...photoEntry} />
+            </div>
+          ))}
+        </div>
+      </section>
+      <section
+        className={`flex justify-center mb-16 px-1.5 transition-opacity duration-500 ease-in-out ${
+          isLoaded && windowWidth < 768
+            ? "opacity-100"
+            : "opacity-0 max-h-0 overflow-hidden"
+        }`}
+      >
+        <div ref={gridRefMobile}>
+          {photoEntries.map((photoEntry, index) => (
+            <div key={index} className="masonry-item">
+              <PhotosTile {...photoEntry} />
+            </div>
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
