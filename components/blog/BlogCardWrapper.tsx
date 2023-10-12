e63e613de85b15c3666ce8dfc69c760d71a9bccd
zimoluo <zimo_luo@outlook.com>
2023-10-12 16:52:29 +0800

    unused reducedAnimation

--- components/blog/BlogCardWrapper.tsx
+++ components/blog/BlogCardWrapper.tsx
@@ -5,9 +5,14 @@ import { getCoverSrc } from "@/lib/blog/util";
 type Props = {
   post: PostData & { unlisted: boolean };
   isVisible: boolean;
+  reducedAnimation?: boolean;
 };
 
-const BlogCardWrapper = ({ post, isVisible }: Props) => {
+const BlogCardWrapper = ({
+  post,
+  isVisible,
+  reducedAnimation = false,
+}: Props) => {
   const [maxHeight, setMaxHeight] = useState("0px");
   const [paddingY, setPaddingY] = useState(isVisible ? "16px" : "0px");
   const [scale, setScale] = useState(isVisible ? "1" : "0.85");
@@ -25,19 +30,26 @@ const BlogCardWrapper = ({ post, isVisible }: Props) => {
     setScale(isVisible ? "1" : "0.85");
   }, [isVisible]);
 
+  const defaultStyle = {
+    maxHeight: isVisible ? maxHeight : "0px",
+    overflow: "hidden",
+    transition:
+      "max-height 0.3s ease-in-out, padding 0.3s ease-in-out, transform 0.3s ease-in-out",
+    paddingTop: paddingY,
+    paddingBottom: paddingY,
+    transform: `scale(${scale})`,
+  };
+
+  const reducedStyle = {
+    maxHeight: isVisible ? maxHeight : "0px",
+    overflow: "hidden",
+    paddingTop: paddingY,
+    paddingBottom: paddingY,
+    opacity: isVisible ? "1" : "0",
+  };
+
   return (
-    <div
-      style={{
-        maxHeight: isVisible ? maxHeight : "0px",
-        overflow: "hidden",
-        transition:
-          "max-height 0.3s ease-in-out, padding 0.3s ease-in-out, transform 0.3s ease-in-out",
-        paddingTop: paddingY,
-        paddingBottom: paddingY,
-        transform: `scale(${scale})`,
-      }}
-      ref={cardRef}
-    >
+    <div style={reducedAnimation ? reducedStyle : defaultStyle} ref={cardRef}>
       <BlogCard
         title={post.title}
         coverImage={getCoverSrc(post.coverImage, post.slug)}
