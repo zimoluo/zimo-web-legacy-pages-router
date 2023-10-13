f39927ed5503ea241d2e2156e77e57ddf57c0174
zimoluo <zimo_luo@outlook.com>
2023-10-13 19:54:40 +0800

    background aria hidden

--- components/BackgroundImage.tsx
+++ components/BackgroundImage.tsx
@@ -1,33 +1,43 @@
-import Image from 'next/image';
-import { backgroundImageMap, ThemeType, blurColorMap } from '../interfaces/themeMaps';
-import { useSettings } from './contexts/SettingsContext';
+import Image from "next/image";
+import {
+  backgroundImageMap,
+  ThemeType,
+  blurColorMap,
+} from "../interfaces/themeMaps";
+import { useSettings } from "./contexts/SettingsContext";
 
 type BackgroundImageProps = {
   theme: ThemeType;
-}
+};
 
 const BackgroundImage: React.FC<BackgroundImageProps> = ({ theme }) => {
-
   const { settings } = useSettings();
-  let backgroundImageSrc = backgroundImageMap[theme] || backgroundImageMap["zimo"];
+  let backgroundImageSrc =
+    backgroundImageMap[theme] || backgroundImageMap["zimo"];
 
-  if (theme === 'about') {
-    backgroundImageSrc = settings.backgroundRichness === 'minimal' ? '/about-pane-opaque.svg' : backgroundImageSrc;
+  if (theme === "about") {
+    backgroundImageSrc =
+      settings.backgroundRichness === "minimal"
+        ? "/about-pane-opaque.svg"
+        : backgroundImageSrc;
   }
 
   const blurSrc = blurColorMap[theme] || blurColorMap["zimo"];
 
   return (
-    <div className={`fixed -z-50 pointer-events-none inset-0 flex items-center justify-center h-screen isolate bg-cover bg-center bg-fixed select-none`}>
+    <div
+      className={`fixed -z-50 pointer-events-none inset-0 flex items-center justify-center h-screen isolate bg-cover bg-center bg-fixed select-none`}
+    >
       <Image
         src={backgroundImageSrc}
-        className='w-full h-full object-cover'
+        className="w-full h-full object-cover"
         height={2000}
         width={1200}
         alt="Background image"
-        placeholder='blur'
+        placeholder="blur"
         priority={true}
         blurDataURL={blurSrc}
+        aria-hidden="true"
       />
     </div>
   );
