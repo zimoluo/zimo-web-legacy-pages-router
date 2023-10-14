dde2d30ffa19bcb56f9b3600012650301771adb9
zimoluo <zimo_luo@outlook.com>
2023-10-14 21:52:07 +0800

    changed how date appears

--- components/about/AboutHeader.tsx
+++ components/about/AboutHeader.tsx
@@ -16,16 +16,14 @@ const AboutHeader: React.FC<Props> = ({
 }) => {
   return (
     <>
-      <div className="flex items-end w-full">
-        <h1
-          className={`font-bold text-4xl ${
-            isIndex ? "text-neutral-900" : "text-sky-900"
-          } leading-relaxed`}
-        >
-          {title}
-        </h1>
-        <p className="flex-grow text-base text-end">{calendarDate(date)}</p>
-      </div>
+      <h1
+        className={`font-bold text-4xl ${
+          isIndex ? "text-neutral-900" : "text-sky-900"
+        } leading-relaxed`}
+      >
+        {title}
+      </h1>
+      <h3 className="text-2xl mt-1">{calendarDate(date)}</h3>
       {description && (
         <p
           className={`text-xl ${
