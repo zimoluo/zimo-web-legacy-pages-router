e2a8a3f8f50fbfac3b096f4983cb457a70f9cda0
zimoluo <zimo_luo@outlook.com>
2023-10-15 18:06:52 +0800

    better questions

--- lib/about/aboutQuestions.ts
+++ lib/about/aboutQuestions.ts
@@ -1,9 +1,8 @@
 export const aboutQuestions = [
   "How can I contact you?",
   "What is this website for?",
-  "Does each page's header have meanings?",
   "How was this website made?",
-  "Why is it *Zimo Web* and not something else?",
+  "Does each page's header have meanings?",
   "Any future plans for this website?",
   "Are accounts safe on this website?",
 ];
@@ -11,9 +10,8 @@ export const aboutQuestions = [
 export const aboutDescriptions = [
   "Contact me via zeemoluo@gmail.com. Should you have any feedback regarding this website, kindly utilize the Feedback Form.",
   "Anything about me. I've always wanted to create some sort of personal playground. It wasn't until I finally had this bit of resolution, the last missing piece in this puzzle.",
-  "Headers are created in under a minute; I sought something cool to be put there, and there it was.",
   "The website, written in TypeScript, is mostly built with Next.js, a React-based framework. Styling is primarily done using Tailwind CSS. Took little over two months to complete, ground up from design and front-end to back-end. Find more of the story behind this website here. A personal note: I have mixed feelings toward React and Node... It's like, there always seems to be some better solution out there that could make life just a bit easier. And yet here we are, stuck riding the coattails of their fame.",
-  "Usually I start projects by naming it straightforward. It's a website, and it's my website, so the project name goes simply by *zimo-web*. Hence, Zimo Web.",
+  "Headers are created in under a minute; I sought something cool to be put there, and there it was.",
   "Time proves to be limited for developing Zimo Web. There's always a ton of stuff to add, and I have to cut some of them to make the initial release happen. Things like a dedicated dark mode and some layout refurbishments. I'd say do expect them in a month or two, if I have more time and fewer conflicting matters.",
   "I'd say mostly. Accounts are established to identify who's sending the comments and, for your convenience, to sync your website settings across devices. Only your name and profile picture from your Google account are utilized, and you retain the option to remove them at any time. I am the only moderator of this website. In the unlikely event that comments become unmanageable, I possess the capability to disable and archive all comments throughout the site. Read more about this website's ~~{Terms of Use}{/about/terms-of-use}~~ and ~~{Privacy Policy}{/about/privacy-policy}~~.",
 ];
