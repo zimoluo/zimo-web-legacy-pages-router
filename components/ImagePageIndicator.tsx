631c7d44db4e32d752d1d7e2c0e3d7db111e8d27
zimoluo <zimo_luo@outlook.com>
2023-10-29 13:28:53 +0800

    better

--- components/ImagePageIndicator.tsx
+++ components/ImagePageIndicator.tsx
@@ -5,14 +5,17 @@ type Props = {
 };
 
 function ImagePageIndicator({ totalPages, currentPage, onPageChange }: Props) {
+  const isCompactView = totalPages > 12;
   return (
-    <div className="flex space-x-1 md:space-x-2">
+    <div
+      className={`flex ${
+        isCompactView ? "space-x-1 md:space-x-2" : "space-x-1.5 md:space-x-2"
+      }`}
+    >
       {Array.from({ length: totalPages }).map((_, index) => (
         <button
           key={index}
-          className={`${
-            totalPages > 12 ? "w-1.5 md:w-2" : "w-2"
-          } rounded-full h-auto aspect-square ${
+          className={`w-1.5 md:w-2 rounded-full h-auto aspect-square ${
             index === currentPage
               ? "bg-neutral-100 bg-opacity-80"
               : "bg-neutral-800 bg-opacity-40"
