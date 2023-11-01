1d1de203afe5e5e6541c01f26183ce33e225e47a
zimoluo <zimo_luo@outlook.com>
2023-11-01 22:17:59 +0800

    trying to fix

--- components/GoogleSignInButton.tsx
+++ components/GoogleSignInButton.tsx
@@ -1,18 +1,16 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { evaluateGoogleAuthCode } from "@/lib/accountClientManager";
 import { useGoogleLogin } from "@react-oauth/google";
 import { useUser } from "./contexts/UserContext";
 import { useSettings } from "./contexts/SettingsContext";
 import Image from "next/image";
-import { ThemeType } from "@/interfaces/themeMaps";
 
-interface Props {
-  theme: ThemeType;
-}
-
-const GoogleSignInButton: React.FC<Props> = ({ theme }) => {
+const GoogleSignInButton: React.FC = () => {
   const { setUser } = useUser();
   const { settings, updateSettingsLocally } = useSettings();
+  const [prompt, setPrompt] = useState("Sign in with Google");
+  const [isPromptVisible, setIsPromptVisible] = useState(true);
+  const promptRef = useRef<HTMLDivElement | null>(null);
 
   const validateCode = async (codeResponse: any) => {
     const codeAuth = codeResponse.code;
@@ -31,10 +29,37 @@ const GoogleSignInButton: React.FC<Props> = ({ theme }) => {
       validateCode(codeResponse);
     },
     flow: "auth-code",
+    onError: () => {
+      handleError();
+    },
   });
 
+  const handleError = () => {
+    if (!promptRef.current) {
+      return;
+    }
+    setIsPromptVisible(false);
+
+    const handleTransitionEnd = () => {
+      if (!promptRef.current) return;
+      promptRef.current.removeEventListener(
+        "transitionend",
+        handleTransitionEnd
+      );
+
+      setPrompt("Open Zimo Web in a Browser to Sign in");
+      setIsPromptVisible(true);
+    };
+
+    promptRef.current.addEventListener("transitionend", handleTransitionEnd);
+  };
+
   return (
-    <button onClick={login} className="flex items-center w-full rounded-full">
+    <button
+      onClick={login}
+      className="flex items-center w-full rounded-full"
+      onError={handleError}
+    >
       <Image
         src="/google-logo.svg"
         className="h-10 md:h-14 w-auto aspect-square"
@@ -42,8 +67,13 @@ const GoogleSignInButton: React.FC<Props> = ({ theme }) => {
         width={64}
         alt="Google's logo"
       />
-      <div className="text-lg md:text-xl ml-2.5 font-bold">
-        Sign in with Google
+      <div
+        className={`text-lg md:text-xl ml-2.5 font-bold transition-opacity duration-300 ease-in-out ${
+          isPromptVisible ? "opacity-100" : "opacity-0"
+        }`}
+        ref={promptRef}
+      >
+        {prompt}
       </div>
     </button>
   );
